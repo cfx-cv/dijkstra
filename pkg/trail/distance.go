a5b2623bd1de8e4a82cc4fca1cc1054200063d1d
Devin Alvaro <devin.alvaro@gmail.com>
2018-08-12 22:43:56 +0700

    Move to DistanceDuration

--- pkg/trail/distance.go
+++ pkg/trail/distance.go
@@ -8,49 +8,55 @@ import (
 	"net/url"
 )
 
+type DistanceDuration struct {
+	Distance `json:"distance"`
+	Duration `json:"duration"`
+}
+
 type Distance struct {
-	Distance int64 `json:"distance"`
-	Duration int64 `json:"duration"`
+	Text  string `json:"text"`
+	Value int64  `json:"value"`
+}
+
+type Duration struct {
+	Text  string `json:"text"`
+	Value int64  `json:"value"`
 }
 
 const (
-	distanceURL = "https://maps.googleapis.com/maps/api/distancematrix/json?"
+	distanceMatrixURL = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 )
 
-func FindDistance(origin, destination Place, apiKey string) (*Distance, error) {
-	url := buildDistanceURL(origin, destination, apiKey)
+func FindDistanceAndDuration(origin, destination Place, apiKey string) (*DistanceDuration, error) {
+	url := buildDistanceMatrixURL(origin, destination, apiKey)
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
 
-	distance, duration, err := parseDistanceResponse(resp)
+	distance, duration, err := parseDistanceMatrixResponse(resp)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Distance{distance, duration}, nil
+	return &DistanceDuration{*distance, *duration}, nil
 }
 
-func buildDistanceURL(origin, destination Place, apiKey string) string {
+func buildDistanceMatrixURL(origin, destination Place, apiKey string) string {
 	u := url.Values{}
 	u.Add("origins", fmt.Sprintf("%f,%f", origin.Latitude, origin.Longitude))
 	u.Add("destinations", fmt.Sprintf("%f,%f", destination.Latitude, destination.Longitude))
 	u.Add("key", apiKey)
-	return distanceURL + u.Encode()
+	return distanceMatrixURL + u.Encode()
 }
 
-func parseDistanceResponse(resp *http.Response) (int64, int64, error) {
+func parseDistanceMatrixResponse(resp *http.Response) (*Distance, *Duration, error) {
 	var body struct {
 		Rows []struct {
 			Elements []struct {
-				Distance struct {
-					Value int64 `json:"value"`
-				} `json:"distance"`
-				Duration struct {
-					Value int64 `json:"value"`
-				} `json:"duration"`
+				Distance `json:"distance"`
+				Duration `json:"duration"`
 
 				Status string `json:"status"`
 			} `json:"elements"`
@@ -60,16 +66,16 @@ func parseDistanceResponse(resp *http.Response) (int64, int64, error) {
 	}
 
 	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
-		return 0, 0, err
+		return nil, nil, err
 	}
 	if status := body.Status; status != "OK" {
-		return 0, 0, errors.New(status)
+		return nil, nil, errors.New(status)
 	}
 	if status := body.Rows[0].Elements[0].Status; status != "OK" {
-		return 0, 0, errors.New(status)
+		return nil, nil, errors.New(status)
 	}
 
-	distance := body.Rows[0].Elements[0].Distance.Value
-	duration := body.Rows[0].Elements[0].Duration.Value
+	distance := &body.Rows[0].Elements[0].Distance
+	duration := &body.Rows[0].Elements[0].Duration
 	return distance, duration, nil
 }
